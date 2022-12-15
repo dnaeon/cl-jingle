5e1dd1486ab02e1ee0875e9f12540f890a37ba23
Marin Atanasov Nikolov <dnaeon@gmail.com>
2022-12-15 18:38:03 +0200

    demo: register HTTP handlers with names

--- demo/api.lisp
+++ demo/api.lisp
@@ -175,16 +175,17 @@ demo."
         new-item))))
 
 (defparameter *urls*
-  `((:method :GET    :path "/api/v1/ping"        :handler ,#'ping-handler)
-    (:method :GET    :path "/api/v1/product"     :handler ,#'get-products-page-handler)
-    (:method :GET    :path "/api/v1/product/:id" :handler ,#'get-product-by-id-handler)
-    (:method :POST   :path "/api/v1/product"     :handler ,#'create-product-handler)
-    (:method :DELETE :path "/api/v1/product/:id" :handler ,#'delete-product-by-id-handler))
+  `((:method :GET    :path "/api/v1/ping"        :handler ,#'ping-handler                 :name "ping")
+    (:method :GET    :path "/api/v1/product"     :handler ,#'get-products-page-handler    :name "get-products-page")
+    (:method :GET    :path "/api/v1/product/:id" :handler ,#'get-product-by-id-handler    :name "get-product-by-id")
+    (:method :POST   :path "/api/v1/product"     :handler ,#'create-product-handler       :name "create-product")
+    (:method :DELETE :path "/api/v1/product/:id" :handler ,#'delete-product-by-id-handler :name "delete-product-by-id"))
   "The URLs map of our API")
 
 (defun register-urls (app)
   (loop :for item :in *urls*
         :for path = (getf item :path)
         :for handler = (getf item :handler)
-        :for method = (getf item :method) :do
-          (setf (jingle:route app path :method method) handler)))
+        :for method = (getf item :method)
+        :for name = (getf item :name) :do
+          (setf (jingle:route app path :method method :identifier name) handler)))
